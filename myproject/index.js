e3928bb8904ceaa4b035e3a17aa9d5a7a2f27010
Ben Hayden <ben@iws-web.com>
2010-03-22 11:41:27 -0500

    I think I'm gitting used to this.

--- myproject/index.js
+++
@@ -1,9 +0,0 @@
-require.paths.push(system.env.DOCUMENT_ROOT + "/..");
-var HTTPResponse = require("jason/core/responder").HTTPResponse;
-
-try {
-    var myResponse = new HTTPResponse(request, response);
-    myResponse.route();
-} catch (e) {
-    response.write("Error: " + e);
-}
