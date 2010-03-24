180aea3b2b0f9e2e27dace6e7b13e31066e1e9b0
extgreen <hayden767@gmail.com>
2010-03-24 16:12:51 -0500

    Fixing a period in config.js

--- myproject/config.js
+++ myproject/config.js
@@ -7,11 +7,11 @@ exports.conf = {
     'db-settings':{
         'database-engine': 'couchdb',
         'database-name': 'spsyndicate',
-        'username':'sp_admin',
-        'password':'intel@)!)Pass',
-        'host':'localhost',
-        'port':'5984'
-    }.
+        'username': 'sp_admin',
+        'password': 'intel@)!)Pass',
+        'host': 'localhost',
+        'port': '5984'
+    },
     'paths': {
         'project': cwd + '/myproject/', 
         'jason': cwd + '/jason/',
