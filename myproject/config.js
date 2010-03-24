56aefc6b8f2e434d30d333da7e9b3c1291a54933
justin@iws-web.com <integral@sentience.localdomain>
2010-03-24 00:46:55 -0500

    added db settings to myproject/config.js for database backends which will be used in controller logic

--- myproject/config.js
+++ myproject/config.js
@@ -4,6 +4,14 @@ exports.conf = {
     'templates': {
         'doctype': '<!DOCTYPE html>',
     },
+    'db-settings':{
+        'database-engine': 'couchdb',
+        'database-name': 'spsyndicate',
+        'username':'sp_admin',
+        'password':'intel@)!)Pass',
+        'host':'localhost',
+        'port':'5984'
+    }.
     'paths': {
         'project': cwd + '/myproject/', 
         'jason': cwd + '/jason/',
